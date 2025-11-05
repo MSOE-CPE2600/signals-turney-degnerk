771d7503b11986916e82d1363a52695d55ead9d4
kdegner <degnerk@msoe.edu>
2025-11-04 22:21:37 -0600

    finished signal send and recieve

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,40 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Kadie Degner
  * 
- * Brief summary of program:
+ * Brief summary of program: Processes a recieved signal with siginfo_t structure to extract sender's PID and sent value.
  */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handle_sigusr1(int sig, siginfo_t *info, void *context) {
+    int value = info->si_value.sival_int;
+    pid_t sender = info->si_pid;
+    printf("\nReceiver: Got SIGUSR1 from PID %d with value %d\n", sender, value);
+}
+
+
+int main() {
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &handle_sigusr1;
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        printf("Receiver: Error setting up signal handler.\n");
+        exit(1);
+    }
+
+    printf("Receiver PID: %d\n", getpid());
+    printf("Receiver: Waiting for SIGUSR1...\n");
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
+
+
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,34 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Kadie Degner
  * 
- * Brief summary of program:
+ * Brief summary of program: Sends a SIGUSR1 signal with a random integer value to a specified process ID.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error: Missing process id argument.\n");
+    }
+
+    pid_t pid = (pid_t)atoi(argv[1]);
+    srand((unsigned)time(NULL));
+    int random_value = rand() % 1000;
+
+    union sigval val;
+    val.sival_int = random_value;
+
+    if (sigqueue(pid, SIGUSR1, val) == -1) {
+        printf("Error sending signal.\n");
+        exit(1);
+    }
+
+    printf("Sender PID: %d\n", getpid());
+    printf("SIGUSR1 signal sent to process %d with value %d\n", pid, random_value);
+    return 0;
+}
